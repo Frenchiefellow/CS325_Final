531f02b566b55314a83173c64733c3d3a3993716
Frenchiefellow <crpeters@umass.edu>
2014-11-19 21:56:02 -0500

    Initial Commit
    Added some basic file structures, bootstrap, a few partials, and
    index.php as a placeholder.

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include '/partials/header.php'; ?> 
+
+<!-- Content --> 
+
+<?php include '/partials/footer.php'; ?> 
---
+++ partials/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+
+<style>
+<?php include '../bootstrap/css/bootstrap.min.css'; ?>
+</style> 
+
+<style>
+<?php include '../bootstrap/css/bootstrap.css'; ?>
+</style> 
+
+
+<head>
+<title>CraigsList</title>
+</head>
+
+
